248a14cbc18efd1bdaf9dd7c172cde859219e121
patt812 <dev.patt812@gmail.com>
2024-08-17 15:35:54 +0900

    add judge

---
+++ internal/game/judge.go
@@ -0,0 +1,37 @@
+package game
+
+type Judge struct {
+	IsCorrectFunc func(string, string, int) bool
+}
+
+func NewJudge() *Judge {
+	return &Judge{
+		IsCorrectFunc: DefaultIsCorrect,
+	}
+}
+
+func DefaultIsCorrect(userInput, targetText string, index int) bool {
+	return userInput[index] == targetText[index]
+}
+
+func (j *Judge) isCorrect(userInput, targetText string, index int) bool {
+	return j.IsCorrectFunc(userInput, targetText, index)
+}
+
+func (j *Judge) IsExit(char rune) bool {
+	// Escape
+	return char == 27
+}
+
+func (j *Judge) ProcessInput(char rune, currentInput, targetText string) string {
+	// Backspace
+	if char == 127 {
+		if len(currentInput) > 0 {
+			return currentInput[:len(currentInput)-1]
+		}
+		return currentInput
+	} else if len(currentInput) < len(targetText) {
+		return currentInput + string(char)
+	}
+	return currentInput
+}
