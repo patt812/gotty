136265471f6f6088881fbd26fcf566d27c031acd
patt812 <dev.patt812@gmail.com>
2024-09-03 20:27:24 +0900

    create log recorder in test/utils

---
+++ test/utils/recorder.go
@@ -0,0 +1,58 @@
+package utils
+
+import (
+	"bytes"
+	"os"
+	"strings"
+	"sync"
+)
+
+type LogRecorder struct {
+	oldStdout *os.File
+	r         *os.File
+	w         *os.File
+	buffer    *bytes.Buffer
+	mu        sync.Mutex
+	started   bool
+}
+
+func NewLogRecorder() *LogRecorder {
+	r, w, err := os.Pipe()
+	if err != nil {
+		panic("Failed to create pipe: " + err.Error())
+	}
+
+	oldStdout := os.Stdout
+	os.Stdout = w
+
+	return &LogRecorder{
+		oldStdout: oldStdout,
+		r:         r,
+		w:         w,
+		buffer:    &bytes.Buffer{},
+		started:   true,
+	}
+}
+
+func (lr *LogRecorder) stop() {
+	lr.mu.Lock()
+	defer lr.mu.Unlock()
+
+	if lr.started {
+		os.Stdout = lr.oldStdout
+		_ = lr.w.Close()
+		_, _ = lr.buffer.ReadFrom(lr.r)
+		lr.started = false
+	}
+}
+
+func (lr *LogRecorder) ToString() string {
+	lr.stop()
+	return lr.buffer.String()
+}
+
+func (lr *LogRecorder) ToArray() []string {
+	lr.stop()
+	output := lr.buffer.String()
+	return strings.Split(strings.TrimSuffix(output, "\n"), "\n")
+}
