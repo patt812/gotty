cd648d6c01d465d598299b83e1b03efdbcae7101
patt812 <38397866+patt812@users.noreply.github.com>
2024-09-03 21:40:40 +0900

    add ANSI convert to test util (#16)
    * add ANSI convert test util
    
    * add ANSI patterns to log recorder

--- test/utils/recorder_test.go
+++ test/utils/recorder_test.go
@@ -6,10 +6,49 @@ import (
 	"testing"
 )
 
-func TestToString(t *testing.T) {
+func TestToAnsiString(t *testing.T) {
 	rec := utils.NewLogRecorder()
 
 	fmt.Println("Hello, World!")
+
+	got := rec.ToAnsiString()
+	if got != "Hello, World!\n" {
+		t.Errorf("Expected 'Hello, World!' in the output, but got: %s", got)
+	}
+}
+
+func TestToAnsiString_MultipleLines(t *testing.T) {
+	rec := utils.NewLogRecorder()
+
+	fmt.Println("Hello, World!")
+	fmt.Println("Goodbye, World!")
+
+	got := rec.ToAnsiString()
+	if got != "Hello, World!\nGoodbye, World!\n" {
+		t.Errorf("Expected 'Hello, World!\nGoodbye, World!' in the output, but got: %s", got)
+	}
+}
+
+func TestToAnsiArray_MultipleLines(t *testing.T) {
+	rec := utils.NewLogRecorder()
+
+	fmt.Println("Hello, World!")
+	fmt.Println("Goodbye, World!")
+
+	got := rec.ToAnsiArray()
+	want := []string{"Hello, World!", "Goodbye, World!"}
+
+	for i, line := range want {
+		if line != got[i] {
+			t.Errorf("Expected '%s' in line %d, but got '%s'", line, i+1, got[i])
+		}
+	}
+}
+
+func TestToString(t *testing.T) {
+	rec := utils.NewLogRecorder()
+
+	fmt.Println("\x1b[31mHello, World!\x1b[0m")
 
 	got := rec.ToString()
 	if got != "Hello, World!\n" {
@@ -17,23 +56,11 @@ func TestToString(t *testing.T) {
 	}
 }
 
-func TestToString_MultipleLines(t *testing.T) {
+func TestToArray(t *testing.T) {
 	rec := utils.NewLogRecorder()
 
-	fmt.Println("Hello, World!")
-	fmt.Println("Goodbye, World!")
-
-	got := rec.ToString()
-	if got != "Hello, World!\nGoodbye, World!\n" {
-		t.Errorf("Expected 'Hello, World!\nGoodbye, World!' in the output, but got: %s", got)
-	}
-}
-
-func TestToArray_MultipleLines(t *testing.T) {
-	rec := utils.NewLogRecorder()
-
-	fmt.Println("Hello, World!")
-	fmt.Println("Goodbye, World!")
+	fmt.Println("\x1b[31mHello, World!\x1b[0m")
+	fmt.Println("\x1b[32mGoodbye, World!\x1b[0m")
 
 	got := rec.ToArray()
 	want := []string{"Hello, World!", "Goodbye, World!"}
